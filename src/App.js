f6093a589baee89bfc8137d58b7ea57ccadd6b02
ivoraleon <iatkinson.leon@gmail.com>
2020-08-17 16:37:50 -0700

    Added Cooking receipe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
+import RecipeList from './RecipeList'
 
 function App() {
-  return <h1>Hello, Atkin!!</h1>
+  return (
+    <RecipeList recipes={sampleRecipes} />
+  )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: "1. Put Salt on Chicken\n2. Put Chicken in oven\n3. Eat Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: "1. Put paprika on Pork\n2. Put Pork in oven\n3. Eat Pork",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      }
+    ]
+  }
+]
 
 export default App;

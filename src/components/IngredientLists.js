b5fd390eb3a687c58f75c17625fc3ff9c34a0e9f
ivoraleon <iatkinson.leon@gmail.com>
2020-08-17 18:14:58 -0700

    Finish Recipe and Ingredient CSS

--- src/components/IngredientLists.js
+++ src/components/IngredientLists.js
@@ -7,7 +7,7 @@ export default function IngredientLists({ ingredients }) {
     })
     
     return (
-        <div>
+        <div className="ingredient-grid">
             {ingredientElements}
         </div>
     )
